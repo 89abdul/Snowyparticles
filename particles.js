980399a5c760ec91b073080e3aaa9cb5a8421351
Abdul Razak Shaikh <89abdul@gmail.com>
2016-05-09 22:17:20 +0530

    Add files via upload
    first version of snowy particles released

---
+++ particles.js
@@ -0,0 +1,71 @@
+
+particlesJS(
+  "dots", {
+    "particles": {
+      "number": {
+        "value": 25,
+        "density": {
+          "enable": true,
+          "value_area": 120
+        }
+      },
+      "color": {
+        "value": "#fff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#fff"
+        }
+      },
+      "opacity": {
+        "value": 0.9,
+        "random": false
+      },
+      "size": {
+        "value": 4,
+        "random": false,
+        "anim": {
+          "enable": true
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 80
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "bottom",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 30,
+          "rotateY": 80
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false
+        }
+      },
+      "modes": {
+        "bubble": {
+          "distance": 200,
+          "size": 25
+        }
+      }
+    },
+    "retina_detect": true
+  });
